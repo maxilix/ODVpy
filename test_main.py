3015d9d66fcc185e632df2cde5a344035399c035
Maxilix <>
2024-05-09 16:53:31 -0400

    test on cp metadata, unk_short seems to be 0 on average

--- test_main.py
+++ test_main.py
@@ -11,24 +11,23 @@ from settings import *
 CONFIG.load()
 
 
-# for level_index in range(26):
+for level_index in range(25):
 
-# filename_we = original_level_filename_we(level_index)
-# level = Level("/home/maxe/PycharmProjects/Desperados Wanted Dead or Alive/data/levels/level_02")
-# motion = level.dvd.move
-#
-# r = []
-# for layer in motion:
-#     for sublayer in layer:
-#         for area in sublayer:
-#             for cp in area:
-#                 r.append(cp.unk_short)
-#
-# rr = [0,0,0,0]
-# for i in range(0, 4):
-#
-#     t = [e[i] for e in r]
-#     rr[i] = sum(t)/len(t)
-#
-# print(rr)
+    level = Level(f"/home/maxe/PycharmProjects/Desperados Wanted Dead or Alive/data/levels/level_{level_index+1:02}")
+    motion = level.dvd.move
+
+    r = []
+    for layer in motion:
+        for sublayer in layer:
+            for area in sublayer:
+                for cp in area:
+                    r.append(cp.unk_short)
+
+    rr = [0,0,0,0]
+    for i in range(0, 4):
+
+        t = [e[i] for e in r]
+        rr[i] = round(sum(t)/len(t),1)
+
+    print(rr)
 
