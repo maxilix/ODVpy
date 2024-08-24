42129080e30f898c608c2d3d3cbe5575b2349ae1
Maxilix <>
2024-08-24 14:59:18 -0400

    MASK integration, WIP

--- dvd/mask.py
+++ dvd/mask.py
@@ -14,34 +14,33 @@ class MaskImage(OdvLeaf):
     point_list_1: list[QPointF] = []
     point_list_2: list[QPointF] = []
     u4: UShort = 0
-    x: UShort
+    layer_id: int
+    position: QPointF
     y: UShort
-    width: UShort
-    height: UShort
 
     @classmethod
-    def from_stream(cls, stream: ReadStream, *, parent) -> Self:
+    def from_stream(cls, stream: ReadStream, *, parent, layer_id) -> Self:
         rop = cls(parent)
+        rop.layer_id = layer_id
         
         rop.flag = stream.read(UChar)
         if rop.flag & 1:
-            nb_coor = stream.read(UShort)
-            rop.point_list_1 = [stream.read(QPointF) for _ in range(nb_coor)]
+            nb_point = stream.read(UShort)
+            rop.point_list_1 = [stream.read(QPointF) for _ in range(nb_point)]
         if rop.flag & 2:
-            nb_coor = stream.read(UShort)
-            rop.point_list_2 = [stream.read(QPointF) for _ in range(nb_coor)]
+            nb_point = stream.read(UShort)
+            rop.point_list_2 = [stream.read(QPointF) for _ in range(nb_point)]
         if rop.flag & 16:
             rop.u4 = stream.read(UShort)
 
-        rop.x = stream.read(UShort)
-        rop.y = stream.read(UShort)
-        rop.width = stream.read(UShort)
-        rop.height = stream.read(UShort)
+        rop.position = stream.read(QPointF)
+        width = stream.read(UShort)
+        height = stream.read(UShort)
 
         mask_length = stream.read(UShort)
         mask_stream = ReadStream(stream.read(Bytes, mask_length))
         data = b''
-        for line_index in range(rop.height):
+        for line_index in range(height):
             line_length = mask_stream.read(UChar)
             line_stream = ReadStream(mask_stream.read(Bytes, line_length))
             while descriptor := line_stream.read(UChar):
@@ -57,10 +56,11 @@ class MaskImage(OdvLeaf):
 
         assert len(mask_stream.read_raw()) == 0
 
-        rop.image = QImage(data, rop.width, rop.height, math.ceil(rop.width/8), QImage.Format.Format_Mono)
+        rop.image = QImage(data, width, height, math.ceil(width/8), QImage.Format.Format_Mono)
         return rop
 
     def to_stream(self, stream: WriteStream) -> None:
+        # TODO write Mask
         pass
 
         
@@ -72,10 +72,10 @@ class Mask(Section, OdvRoot):
 
     def _load(self, substream: ReadStream):
         nb_layer = substream.read(UShort)
-        for layer_index in range(nb_layer):
+        for layer_id in range(nb_layer):
             nb_mask = substream.read(UShort)
             for mask_index in range(nb_mask):
-                self.add_child(substream.read(MaskImage, parent=self))
+                self.add_child(substream.read(MaskImage, parent=self, layer_id=layer_id))
 
     def _save(self, substream: WriteStream) -> None:
         pass
--- qt/control/tab_mask.py
+++ qt/control/tab_mask.py
@@ -2,28 +2,38 @@ from PyQt6.QtGui import QColor
 
 from dvd.mask import Mask, MaskImage
 from qt.control.inspector_abstract import Inspector
+from qt.control.inspector_generic import InfoSubInspector
 from qt.control.inspector_graphic import MaskImageSubInspector, GeometrySubInspector
 from qt.control.tab__abstract import QTabControlGenericTree
 from qt.graphics import GraphicMultiLine
 
 
 class MaskImageInspector(Inspector):
+    odv_object: MaskImage
     deletable = True
     child_name = ""  # cannot add child
 
     def init_sub_inspector(self):
         self.sub_inspector_group["Bitmap"] = [(msi:=MaskImageSubInspector(self, "image"))]
         msi.opacity_slider.setValue(40)
-        msi.visibility_checkbox.setChecked(True)
-        msi.graphic.setPos(self.odv_object.x, self.odv_object.y)
+        msi.graphic.setPos(self.odv_object.position)
 
-        self.sub_inspector_group["Polygon"] = []
+        self.sub_inspector_group["Info"] = [
+            InfoSubInspector(self, "layer_id", "layer_id"),
+            InfoSubInspector(self, "flag", "flag"),
+            InfoSubInspector(self, "u4", "u4"),
+
+        ]
+
+
+
+        self.sub_inspector_group["Multiline"] = []
         if self.odv_object.point_list_1 != []:
-            self.sub_inspector_group["Polygon"] += [GeometrySubInspector(self, "point_list_1", "L1", graphic_type=GraphicMultiLine, color=QColor(0, 255, 0))]
+            self.sub_inspector_group["Multiline"] += [GeometrySubInspector(self, "point_list_1", "L1", graphic_type=GraphicMultiLine, color=QColor(0, 255, 0))]
         if self.odv_object.point_list_2 != []:
-            self.sub_inspector_group["Polygon"] += [GeometrySubInspector(self, "point_list_2", "L2", graphic_type=GraphicMultiLine, color=QColor(0, 0, 255))]
-        if self.sub_inspector_group["Polygon"] == []:
-            self.sub_inspector_group.pop("Polygon")
+            self.sub_inspector_group["Multiline"] += [GeometrySubInspector(self, "point_list_2", "L2", graphic_type=GraphicMultiLine, color=QColor(0, 0, 255))]
+        if self.sub_inspector_group["Multiline"] == []:
+            self.sub_inspector_group.pop("Multiline")
 
 
 
@@ -32,18 +42,7 @@ class MaskInspector(Inspector):
     child_name = "Mask Image"
 
     # def new_odv_child(self):
-    #     new_lift_area = LiftArea(self.odv_object)
-    #     new_lift_area.move = self.odv_object.move
-    #
-    #     new_lift_area.lift_type = 1
-    #     new_lift_area.main_area = None
-    #     new_lift_area.main_area_below = None
-    #     new_lift_area.main_area_above = None
-    #     new_lift_area.gateway_below = self._tab_control.scene.new_centered_gateway(scale=0.2)
-    #     new_lift_area.gateway_above = self._tab_control.scene.new_centered_gateway(scale=0.2)
-    #     new_lift_area.perspective = 0
-    #
-    #     return new_lift_area
+
 
 
 class QMaskTabControl(QTabControlGenericTree):
